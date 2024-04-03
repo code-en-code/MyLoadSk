cc66e5bb73745c5f036b979ee02cd9e886f2f398
DESKTOP-2B6FLDM\Valkennedy <antoniovalkennedy@gmail.com>
2024-04-03 14:18:44 -0300

    Implemented the library's initial journey.

--- samples/appform/appform.dpr
+++ samples/appform/appform.dpr
@@ -2,12 +2,14 @@ program appform;
 uses
   System.StartUpCopy,
   FMX.Forms,
+  FMX.Skia,
   index in 'index.pas' {IndexView},
   mythread in '..\..\modules\mythread\src\mythread.pas',
   myloadsk in '..\..\src\myloadsk.pas';
 
 {$R *.res}
 begin
+  GlobalUseSkia := True;
   Application.Initialize;
   Application.CreateForm(TIndexView, IndexView);
   Application.Run;
